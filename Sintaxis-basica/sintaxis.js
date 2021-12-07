7965c251dfd9ded7912e4c7989a6044409e29e9c
JoshDrck <joseph.64@outlook.com>
2021-12-06 21:40:41 -0500

    Sintaxis basica

--- Sintaxis-basica/sintaxis.js
+++ Sintaxis-basica/sintaxis.js
@@ -1,14 +1,94 @@
 // Valores en JavaScript
 
+40;
+
+("Joseph Alejandro");
+
+true;
+false;
+
+null;
+undefined[(1, 2, 3)];
+
+{
+  nombre: "Joseph";
+}
+
 // Declaración de variables
+
+var nombre = "Joseph";
+
+// declarar
+var edad;
+// inicializar
+edad = 22;
+// resultado
+edad;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: "Joseph",
+  apellido: "Alejandro",
+  edad: 22,
+};
 
 // Funciones
 
-    // declarativas
-    // expresion
+// declarativas
+
+function miFuncion() {
+  return 3;
+}
+
+// expresiones
+
+var miFuncion2 = function (a, b) {
+  return a + b;
+};
+
+miFuncion2(1, 2);
 
 // Hoisting
+
+console.log(miNombre);
+
+var miNombre = "Joseph";
+
+hey();
+
+function hey() {
+  console.log("Hola" + miNombre);
+}
 
 // Switch
 
 // Bucles
+let numero = 10;
+
+// for
+for (let index = 0; index < numero; index++) {
+  console.log("Estoy haciendo un bucle :D " + index);
+}
+
+// While
+
+while (numero > 1) {
+  console.log("Estoy haciendo un bucle :D " + numero);
+  numero--;
+}
+
+// Do-While
+
+do {
+  console.log("Estoy haciendo un bucle :D" + numero);
+  nombre--;
+} while (10 < 1);
+
+// Estructuras
+
+
+// objeto JSON
+
+
+// LocalStorage
